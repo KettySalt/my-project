8a9f80cc277e1a3a07c006ab9b94f0cd6a52cb76
Ketty <yuriy.sev51@gmail.com>
2022-12-12 12:13:10 +0200

    Changed style of navbar font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 let navEl = document.querySelector(".navbar");
-window.addEventListener("scroll", () => {
-  console.log("Scrolling...");
+let brandEl = document.querySelector(".navbar-brand");
+let linkAbout = document.querySelector("#homepage-about");
+let linkContact = document.querySelector("#homepage-contact");
 
-  if (window.scrollY >= 150) {
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 200) {
     navEl.classList.add("nav-scrolled");
-  } else if (window.scrollY < 150) {
+    brandEl.classList.add("navbar-fontstyle");
+    linkAbout.classList.add("navbar-fontstyle");
+    linkContact.classList.add("navbar-fontstyle");
+  } else if (window.scrollY < 200) {
     navEl.classList.remove("nav-scrolled");
+    brandEl.classList.remove("navbar-fontstyle");
+    linkAbout.classList.remove("navbar-fontstyle");
+    linkContact.classList.remove("navbar-fontstyle");
   }
 });
