d528c9e1adbb6ab173df5095fc057550fe2b8697
Ketty <yuriy.sev51@gmail.com>
2022-12-14 15:43:25 +0200

    Added new page Work

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ let navEl = document.querySelector(".navbar");
 let brandEl = document.querySelector(".navbar-brand");
 let linkAbout = document.querySelector("#homepage-about");
 let linkContact = document.querySelector("#homepage-contact");
+let linkWork = document.querySelector("#homepage-work");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 200) {
@@ -9,10 +10,12 @@ window.addEventListener("scroll", () => {
     brandEl.classList.add("navbar-fontstyle");
     linkAbout.classList.add("navbar-fontstyle");
     linkContact.classList.add("navbar-fontstyle");
+    linkWork.classList.add("navbar-fontstyle");
   } else if (window.scrollY < 200) {
     navEl.classList.remove("nav-scrolled");
     brandEl.classList.remove("navbar-fontstyle");
     linkAbout.classList.remove("navbar-fontstyle");
     linkContact.classList.remove("navbar-fontstyle");
+    linkWork.classList.remove("navbar-fontstyle");
   }
 });
